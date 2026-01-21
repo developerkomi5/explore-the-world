f22db5926f762b43dd19a888270dabb78cd83c15
Komal Buddhdev <developerkomi5@gmail.com>
2026-01-21 13:37:48 +0530

    Fix SPA routing and Vercel blank page issue

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
+import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import { AppProvider } from "./context/AppContext.jsx";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import { AppProvider } from "./context/AppContext";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <AppProvider>
-    <App />
-  </AppProvider>
+  <React.StrictMode>
+    <BrowserRouter>
+      <AppProvider>
+        <App />
+      </AppProvider>
+    </BrowserRouter>
+  </React.StrictMode>
 );
